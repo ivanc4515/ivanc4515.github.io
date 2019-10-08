80f6e58839f56e34d2229895a34a498173fc8903
ivanc4515 <56265105+ivanc4515@users.noreply.github.com>
2019-10-08 11:10:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownername = "Ivan C."
+let username = "ivanc4515";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
